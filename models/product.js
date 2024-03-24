51d2a521ccca57a9321e5902901a2657f034dea0
wayne23123 <wayne23123@gmail.com>
2024-03-24 22:15:15 +0800

    103.基本MVC

--- models/product.js
+++ models/product.js
@@ -1,6 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (callback) => {
+  //   const p = path.join(
+  //     path.dirname(process.mainModule.filename),
+  //     'data',
+  //     'products.json'
+  //   );
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    }
+    callback(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
@@ -8,43 +28,52 @@ module.exports = class Product {
 
   // function save() {
   save() {
-    // products.push(this);
-    // 101. storing data in files via the model
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      // 故意用箭頭函示，讓他失去 this 指向
+    // 103.
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
+    // // products.push(this);
+    // // 101. storing data in files via the model
+    // const p = path.join(
+    //   path.dirname(process.mainModule.filename),
+    //   'data',
+    //   'products.json'
+    // );
+    // fs.readFile(p, (err, fileContent) => {
+    //   let products = [];
+    //   if (!err) {
+    //     products = JSON.parse(fileContent);
+    //   }
+    //   // 故意用箭頭函示，讓他失去 this 指向
+    //   products.push(this);
+    //   fs.writeFile(p, JSON.stringify(products), (err) => {
+    //     console.log(err);
+    //   });
+    // });
   }
 
   static fetchAll(callback) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    // 這是異步代碼
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        // return [];
-        // 102. 解決異步問題
-        callback([]);
-      }
-      //   return JSON.parse(fileContent);
-      // 102. 解決異步問題
-      callback(JSON.parse(fileContent));
-    });
-    // return products;
+    // 103.
+    getProductsFromFile(callback);
+    // const p = path.join(
+    //   path.dirname(process.mainModule.filename),
+    //   'data',
+    //   'products.json'
+    // );
+    // // 這是異步代碼
+    // fs.readFile(p, (err, fileContent) => {
+    //   if (err) {
+    //     // return [];
+    //     // 102. 解決異步問題
+    //     callback([]);
+    //   }
+    //   //   return JSON.parse(fileContent);
+    //   // 102. 解決異步問題
+    //   callback(JSON.parse(fileContent));
+    // });
+    // // return products;
   }
 };
