5c7a7e0dbb93d336b60d149d4dd57763bdb51bbd
wayne23123 <wayne23123@gmail.com>
2024-03-24 21:38:57 +0800

    101. storing data in files via the model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
   constructor(t) {
@@ -7,10 +8,38 @@ module.exports = class Product {
 
   // function save() {
   save() {
-    products.push(this);
+    // products.push(this);
+    // 101. storing data in files via the model
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      // 故意用箭頭函示，讓他失去 this 指向
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
+    // return products;
   }
 };
