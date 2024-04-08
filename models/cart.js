e2ab6f18c15a2d32c2bc7c60caaf42adc3aaa09d
wayne23123 <wayne23123@gmail.com>
2024-04-08 22:12:32 +0800

    sql介紹

--- models/cart.js
+++ models/cart.js
@@ -39,16 +39,24 @@ module.exports = class Cart {
     });
   }
 
-  static deleteProduct(id, product) {
+  static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
       if (err) {
         return;
       }
       const updatedCart = { ...JSON.parse(fileContent) };
 
-      const product = updatedCart.products.findIndex((prod) => {
+      const product = updatedCart.products.find((prod) => {
         return prod.id === id;
       });
+
+         // const product = updatedCart.products.findIndex((prod) => {
+      //   return prod.id === id;
+      // });
+
+      if(!product) {
+        return
+      }
 
       const productQty = product.qty;
 
@@ -56,7 +64,7 @@ module.exports = class Cart {
         return prod.id !== id;
       });
 
-      updatedCart.totalPrice = cart.totalPrice - productPrice * productQty;
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
 
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
